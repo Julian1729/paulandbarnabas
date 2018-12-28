0a3ea06fefa0b1b68d6f2da7994fa25bb5b89678
Julian1729 <hernandez.julian17@gmail.com>
2018-12-28 14:41:31 -0500

    Wipe and seed all collections on app start

--- dev/seed/Seed.js
+++ dev/seed/Seed.js
@@ -6,6 +6,8 @@ const CongregationModel = require('../../models/Congregation');
 const CongregationSeed = require('./Congregation');
 const UserModel = require('../../models/User');
 const UserSeed = require('./User');
+const TerritoryModel = require('../../models/Territory');
+const TerritorySeed = require('./Territory');
 const logger = require('../../utils/logger');
 const Utils = require('../../utils/utils');
 
@@ -18,9 +20,14 @@ Utils.clearCollection(CongregationModel)
     logger.debug('Congregation collection cleared');
     return Utils.clearCollection(UserModel)
   })
-  // User collection cleared
+  // Clear User collection
   .then(r => {
     logger.debug('User collection cleared')
+    return Utils.clearCollection(TerritoryModel);
+  })
+  // Clear Territory collection
+  .then(r => {
+    logger.debug('Territory collection cleared');
     return CongregationModel.create(CongregationSeed);
   })
   // Save congregation
@@ -30,13 +37,23 @@ Utils.clearCollection(CongregationModel)
     // Set congregation Id for users to first congregation entered
     UserSeed.forEach(user => {
       user.congregation = congregations[0]._id;
-    })
+    });
     return UserModel.create(UserSeed);
   })
   // Save Users
   .then(users => {
     logger.debug(`${users.length} users entered into db`)
     insertedData.users = users;
+    // Add congregation to territory obj
+    TerritorySeed.forEach(territory => {
+      territory.congregation = insertedData.congregations[0]._id;
+    });
+    return TerritoryModel.create(TerritorySeed);
+  })
+  // Save Territory
+  .then(territories => {
+    logger.debug(`${territories.length} territories entered into db`);
+    insertedData.territories = territories;
   })
   .catch(e => {
     logger.debug(e.stack);
