e98c0cd624232cdd3f0dc09d10da4b342453e307
Julian1729 <hernandez.julian17@gmail.com>
2019-03-05 14:43:40 -0500

    Add support for subunits in unit route and UI

--- controllers/Territory/Unit.js
+++ controllers/Territory/Unit.js
@@ -19,8 +19,27 @@ middleware.findRequestedUnit = (req, res, next) => {
   let unitNumber = req.params.unit_number;
   let block_ref = req.app.locals.territory.current.block;
   let unit = block_ref.block.unit(unitNumber);
+  let subunitParam = req.query.subunit || null;
 
+  // attach to locals
   req.app.locals.territory.current.unit = unit;
+
+  // get subunit if "subunit" GET param
+  if(subunitParam){
+    // search for subunit in unit
+    try {
+      let subunit = unit.findSubunit(subunitParam);
+      // attach to locals
+      req.app.locals.territory.current.subunit = subunit;
+    } catch (e) {
+      // send 404 if subunit not found
+      if(e instanceof errors.SubunitNotFound){
+       return res.status(HttpStatus.NOT_FOUND).send();
+      }
+      // if not delegate to main error handler
+      throw e;
+    }
+  }
 
   return next();
 
@@ -35,13 +54,15 @@ endpoints.overview = (req, res, next) => {
   let fragment = req.app.locals.territory.current.fragment;
   let block_ref = req.app.locals.territory.current.block;
   let unit = req.app.locals.territory.current.unit;
+  let subunit = req.app.locals.territory.current.subunit || null;
 
   let renderVars = {
     fragment: {
       number: fragment.number
     },
     block_ref,
-    unit
+    unit,
+    subunit
   };
 
   res.render('Fragment/Unit', renderVars);
