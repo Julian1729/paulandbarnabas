30796f68800184c1aef66ee59ab6c50f39652f60
Julian1729 <hernandez.julian17@gmail.com>
2019-05-22 22:31:34 -0400

    Add validators to index

--- utils/index.js
+++ utils/index.js
@@ -5,10 +5,13 @@ const logger = require('./logger.util');
 
 const helpers = require('./helpers.util');
 
+const validators = require('./validators');
+
 const PBURLConstructor = require('./URLConstructor.util');
 
 module.exports = {
   logger,
   helpers,
-  PBURLConstructor
+  validators,
+  PBURLConstructor,
 };
