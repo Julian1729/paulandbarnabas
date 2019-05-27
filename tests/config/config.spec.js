08393aaaa257011c529472df9b03e4a950cd65a2
Julian1729 <hernandez.julian17@gmail.com>
2019-05-26 22:01:24 -0400

    Refactor config test

--- tests/config/config.spec.js
+++ tests/config/config.spec.js
@@ -1,16 +1,32 @@
 const expect = require('expect.js');
 const appRoot = require('app-root-path');
 
+const envVars = require(`${appRoot}/config/env_vars`);
+
 describe('Configuration', () => {
 
-  /**
-   * Reset NODE_ENV to emulate the start of execution,
-   * and delete config from cache to cause reload
-   */
+  let ogEnv = null;
+
+  before(() => {
+
+    // store initial environment
+    ogEnv = process.env.NODE_ENV;
+
+  });
+
   beforeEach(() => {
-    // FIXME: this does not delete all added
-    // env vars before each test
+    // assure all added process.env properties are deleted
+    let environments = Object.entries(envVars);
+    for (env of environments) {
+      let props = env[1];
+      for (prop of Object.keys(props)) {
+        delete process.env[prop];
+        expect(process.env[prop]).to.equal(undefined);
+      }
+    }
+    // reset env
     process.env.NODE_ENV = '';
+    // unload config file
     delete require.cache[require.resolve(`${appRoot}/config/config`)];
   });
 
@@ -27,12 +43,19 @@ describe('Configuration', () => {
     expect(process.env.NODE_ENV).to.eql('development');
   });
 
-  it('should set testing variables', () => {
+  it('should set test variables', () => {
     process.env.NODE_ENV = 'test';
     expect(process.env.NODE_ENV).to.eql('test');
     const config = require(`${appRoot}/config/config`);
     expect(process.env.NODE_ENV).to.eql('test');
     expect(process.env.MONGODB_URI).to.eql('mongodb://localhost:27017/PaulAndBarnabasTesting');
+  });
+
+  after(() => {
+
+    // reset env to initial value
+    process.env.NODE_ENV = ogEnv;
+
   });
 
 });
